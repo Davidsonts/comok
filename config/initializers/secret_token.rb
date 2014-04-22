94a17f23eb3b03155e9e6a3b5e305800a04f0002
Davidson567 <davidsontrabalho@gmail.com>
2014-04-22 20:01:30 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comok::Application.config.secret_key_base = '627a899fdc18ca11e25749a11b6de65f98507d808d83c57a34298d4b6f6f4cf7500cff074012ac55d547fd7dd675fe0909739b6ad7ea6584969a008b72884c88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Comok::Application.config.session_store :cookie_store, key: '_comok_session'

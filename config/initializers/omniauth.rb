5f9403cf0e5d53da910edcc611cfb5a0d1f585af
Davidson567 <davidsontrabalho@gmail.com>
2014-04-22 20:26:30 -0300

    projeto beta1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :facebook, '1429993790583401', 'f24dd6735f6efe920f5022018e97141b'
 end

ed511c71997f63fc000a42e7e0e771a82e514911
Eric <sistemasung012@gmail.com>
2014-05-06 20:33:35 -0300

    Associações: associação has_and_belongs_to_many entre Resource e Type_event

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,4 @@
 class Resource < ActiveRecord::Base
-has_many :events
+  has_many :events
+  has_and_belongs_to_many :type_events
 end
--- app/models/type_event.rb
+++ app/models/type_event.rb
@@ -1,3 +1,4 @@
 class TypeEvent < ActiveRecord::Base
-has_many :events
+  has_many :events
+  has_and_belongs_to_many :resources
 end
